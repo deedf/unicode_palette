98cd46e847382b775cc3783532978bb9c5a9bb5a
Didier Frick <didier@dfr.ch>
2023-12-16 12:49:52 +0100

    add hover flag

--- unicode_palette.py
+++ unicode_palette.py
@@ -4,7 +4,7 @@
 import argparse
 import base64
 import sys
-from typing import Generator, Optional, TextIO, Tuple
+from typing import Generator, TextIO, Tuple
 import unicodedata
 import urllib.parse
 
@@ -17,6 +17,13 @@ def _create_arg_parser() -> argparse.ArgumentParser:
         "--add-name",
         type=bool,
         help="Add character name",
+        default=False,
+        action=argparse.BooleanOptionalAction,
+    )
+    result.add_argument(
+        "--add-hover",
+        type=bool,
+        help="Add character name as hover",
         default=False,
         action=argparse.BooleanOptionalAction,
     )
@@ -57,44 +64,47 @@ UNICODE_ZWS = "\u200B"
 
 def _char_gen(
     args: argparse.Namespace,
-) -> Generator[Tuple[str, Optional[str]], None, None]:
+) -> Generator[Tuple[str, str], None, None]:
     categories = frozenset(args.category)
     for code in range(UNICODE_END):
         u = chr(code)
         if unicodedata.category(u) in categories:
             yield (
                 u,
-                unicodedata.name(u).title() if args.add_name else None,
+                unicodedata.name(u).title(),
             )
 
 
-def _html_string(t: Tuple[str, Optional[str]]) -> str:
-    return t[0] + "".join(
-        [
-            f"<span class='{i}'>{t[i]}</span>"
-            for i in range(1, len(t))
-            if t[i] is not None
-        ]
+def _html_string(t: Tuple[str, str], add_name: bool, add_hover: bool) -> str:
+    return (f"<span title='{t[1]}'>{t[0]}</span>" if add_hover else t[0]) + (
+        f"<span class='n'>{t[1]}</span>" if add_name else ""
     )
 
 
-def _text_string(t: Tuple[str, Optional[str]]) -> str:
-    return " ".join([s for s in t if s is not None])
+def _text_string(t: Tuple[str, str], add_name: bool) -> str:
+    return t[0] + (UNICODE_ZWS + t[1]) if add_name else ""
 
 
 def _write_output(args: argparse.Namespace, out: TextIO):
     if args.html:
         out.write("data:text/html;charset=UTF-8;")
         out_str = (
-            "<html><head><title>Unicode Palette</title><style> span {font-size:"
+            "<html><head><title>Unicode Palette</title><style> .n {font-size:"
             + args.name_font_size
             + ";} </style></head><body>"
-            + "".join([_html_string(t) for t in _char_gen(args)])
+            + "".join(
+                [
+                    _html_string(t, args.add_name, args.add_hover)
+                    for t in _char_gen(args)
+                ]
+            )
             + "</body></html>"
         )
     else:
         out.write("data:text/plain;charset=UTF-8;")
-        out_str = "".join([_text_string(t) for t in _char_gen(args)])
+        out_str = UNICODE_ZWS.join(
+            [_text_string(t, args.add_name) for t in _char_gen(args)]
+        )
     if args.base64:
         out.write("base64,")
         b = base64.b64encode(out_str.encode("utf-8"))
